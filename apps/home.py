2b0345c99898ba5e38fc6439129f94e58a678cdb
Akshat Kumar Jain <40888828+Akshat-kumar-jain@users.noreply.github.com>
2022-03-11 00:26:14 +0530

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -9,7 +9,7 @@ def app():
         """
     A [streamlit](https://streamlit.io) app template for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu). 
     To create a direct link to a pre-selected menu, add `?page=<app name>` to the URL, e.g., `?page=upload`.
-    https://share.streamlit.io/giswqs/streamlit-template?page=upload
+    https://github.com/Akshat-kumar-jain/GeoSpatial_Stramlit?page=upload
 
     """
     )
