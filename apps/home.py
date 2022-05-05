38276df56a856c70978fbc11edc471a0df3bd7e0
Akshat Kumar Jain <40888828+Akshat-kumar-jain@users.noreply.github.com>
2022-05-05 22:58:16 +0530

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -24,9 +24,9 @@ def app():
     )
 
     
-        st.image("https://github.com/Akshat-kumar-jain/GeoSpatial_Stramlit/blob/main/132a2fee227448bf0e55995718e0fb77-89a1f02b89cab10ef8f63e140be40dde_getPixels%20(4).gif")
-        st.image("https://github.com/Akshat-kumar-jain/GeoSpatial_Stramlit/blob/main/6cab6d96-4f7d-4819-a31e-ada7e283622f%20(1).gif")
+    st.image("https://github.com/Akshat-kumar-jain/GeoSpatial_Stramlit/blob/main/132a2fee227448bf0e55995718e0fb77-89a1f02b89cab10ef8f63e140be40dde_getPixels%20(4).gif")
+    st.image("https://github.com/Akshat-kumar-jain/GeoSpatial_Stramlit/blob/main/6cab6d96-4f7d-4819-a31e-ada7e283622f%20(1).gif")
 
     
-        st.image("https://github.com/giswqs/data/raw/main/timelapse/goes.gif")
-        st.image("https://github.com/giswqs/data/raw/main/timelapse/fire.gif")
+    st.image("https://github.com/giswqs/data/raw/main/timelapse/goes.gif")
+    st.image("https://github.com/giswqs/data/raw/main/timelapse/fire.gif")
