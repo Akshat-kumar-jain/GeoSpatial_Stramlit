63f08d6567f494cb60fc06c3d17f9094e8a7b616
Akshat Kumar Jain <40888828+Akshat-kumar-jain@users.noreply.github.com>
2022-05-05 22:52:04 +0530

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -9,9 +9,7 @@ def app():
         """
         This multi-page web app demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and open-source mapping libraries, 
         such as [leafmap](https://leafmap.org), [geemap](https://geemap.org), [pydeck](https://deckgl.readthedocs.io), and [kepler.gl](https://docs.kepler.gl/docs/keplergl-jupyter).
-        This is an open-source project and you are very welcome to contribute your comments, questions, resources, and apps as [issues](https://github.com/giswqs/streamlit-geospatial/issues) or 
-        [pull requests](https://github.com/giswqs/streamlit-geospatial/pulls) to the [GitHub repository](https://github.com/giswqs/streamlit-geospatial).
-
+        
         """
     )
 
@@ -26,7 +24,7 @@ def app():
 
     row1_col1, row1_col2 = st.columns(2)
     with row1_col1:
-        st.image("https://github.com/giswqs/data/raw/main/timelapse/spain.gif")
+        st.image("https://github.com/Akshat-kumar-jain/GeoSpatial_Stramlit/blob/main/132a2fee227448bf0e55995718e0fb77-89a1f02b89cab10ef8f63e140be40dde_getPixels%20(4).gif")
         st.image("https://github.com/giswqs/data/raw/main/timelapse/las_vegas.gif")
 
     with row1_col2:
